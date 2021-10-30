f24192416c0692ba85eb385396982d9c81f82d88
naeFAe <82336045+naeFAe@users.noreply.github.com>
2021-10-30 23:25:01 +0300

    Create main.cpp

---
+++ angajat.cpp
@@ -0,0 +1,23 @@
+
+#include "angajat.h"
+
+
+angajat::angajat(std::string nume, const std::string &prenume, const std::string &gen) : nume(std::move(nume)),
+                                                                                         prenume(prenume),
+                                                                                         gen(gen) {}
+
+std::ostream &operator<<(std::ostream &out, const angajat &angajat) {
+    angajat.print(out);
+    return out;
+}
+
+void angajat::print(std::ostream &out) const {
+    const auto &angajat=*this;
+    out<<"nume: "<<nume<<" "<<"prenume: "<<prenume <<" "<<"gen :"<<gen<<" "<<"salariu: "<< salariu;
+}
+
+
+
+
+
+
---
+++ manager.cpp
@@ -0,0 +1,58 @@
+
+
+#include "manager.h"
+#include "agent_inchirieri.h"
+
+
+
+manager::manager(const std::string &nume, const std::string &prenume, const std::string &gen) : angajat(nume, prenume, gen) {}
+
+void manager::print(std::ostream &out) const {
+    angajat::print(out);
+    out<<" "<<"nr angajati:" <<nr_angajati<<" ";
+}
+
+std::shared_ptr<angajat> manager::clone() const {
+    return std::shared_ptr<angajat>();
+}
+
+
+void manager::adauga2(const agent_inchirieri &agent1) {
+    nr_angajati=nr_angajati+1;
+    agenti.push_back(agent1);
+}
+
+manager::manager(const manager &copie) : nr_angajati(copie.nr_angajati){
+    for(const auto &j : copie.angajati)
+        angajati.push_back(j->clone());
+}
+
+void manager::adauga(const angajat &angajat1) {
+    nr_angajati=nr_angajati+1;
+    angajati.push_back(angajat1.clone());
+}
+
+manager &manager::operator=(manager &copie) {
+    swap(*this, copie);
+    return *this;
+}
+
+void manager::swap(manager &m1, manager &m2) {
+    using std::swap;
+    swap(m1.nr_angajati,m2.nr_angajati);
+    swap(m1.angajati,m2.angajati);
+}
+
+void manager::calc_salariu() {
+    salariu=5000;
+    for (auto &agent : agenti)
+        salariu = salariu + agent.getNrContracte()*20;
+}
+
+void manager::afisare_subalterni() {
+    for(auto &agent : agenti){
+        agent.print(std::cout);
+    }
+}
+
+
